611c4aecbf050a8aae12847788ee1576b1cc80b5
vincilisi <96939594+vincilisi@users.noreply.github.com>
2024-07-21 15:39:16 +0200

    Add files via upload

---
+++ js1.js
@@ -0,0 +1,5 @@
+let anno_corrente= 2024;
+let anno_di_nascita= 1998
+let eta= anno_corrente-anno_di_nascita;
+let m100= 100 - eta;
+console.log("Hai "+eta+ " anni, "+"ti mancano, "+m100+" per compierne 100");
